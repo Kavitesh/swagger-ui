400c26419f6cc49335ac561097030fb5ea8e0ae4
Kamboj Kavitesh <10054645@gemalto.com>
2017-12-19 14:16:10 +0530

    KKA - Temprary commit with black side bar

--- src/core/components/operations.jsx
+++ src/core/components/operations.jsx
@@ -13,7 +13,7 @@ export default class Operations extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { searchValue: '', searching: false , searchPlaceHolder:"Search"};
+    this.state = { searchValue: '', searching: false, searchPlaceHolder: "Search" };
 
     this.handleSearch = this.handleSearch.bind(this);
     this.handleSearchClose = this.handleSearchClose.bind(this);
@@ -23,27 +23,55 @@ export default class Operations extends React.Component {
 
   handleSearch(event) {
     if (event.target.value.length > 2) {
-      this.setState({ searchValue: event.target.value, searching: true, searchPlaceHolder:"" });
+      this.setState({ searchValue: event.target.value, searching: true, searchPlaceHolder: "" });
     } else {
-      this.setState({ searchValue: event.target.value, searching: false , searchPlaceHolder:""});
+      this.setState({ searchValue: event.target.value, searching: false, searchPlaceHolder: "" });
     }
   }
 
   handleSearchClose(event) {
-    this.setState({ searchValue: '', searching: false, searchPlaceHolder:"Search" });
+    this.setState({ searchValue: '', searching: false, searchPlaceHolder: "Search" });
   }
 
-  handleSearchFocus(event){
-    this.setState({ searchValue: this.state.searchValue, searching: this.state.searching, searchPlaceHolder:"" });
+  handleSearchFocus(event) {
+    this.setState({ searchValue: this.state.searchValue, searching: this.state.searching, searchPlaceHolder: "" });
   }
 
-  handleSearchBlur(event){
-    if(this.state.searching){
-      this.setState({ searchValue: this.state.searchValue, searching: this.state.searching, searchPlaceHolder:"Search" });
-    }else{
-      this.setState({ searchValue: "", searching: this.state.searching, searchPlaceHolder:"Search" });
+  handleSearchBlur(event) {
+    if (this.state.searching) {
+      this.setState({ searchValue: this.state.searchValue, searching: this.state.searching, searchPlaceHolder: "Search" });
+    } else {
+      this.setState({ searchValue: "", searching: this.state.searching, searchPlaceHolder: "Search" });
     }
-    
+
+  }
+
+  showContent(key, id, activeId) {
+    var all = document.getElementsByClassName("opblock-show");
+    for (var i = 0; i < all.length; i++) {
+      if (!all[i].classList.contains("opblock-hidden")) {
+        all[i].classList.add("opblock-hidden");
+      }
+    }
+    var x;
+    if (key == "info" || key == "models") {
+      x = document.getElementsByClassName(key)[0];
+      history.replaceState({}, key, `?${key}`);
+    } else {
+      x = document.getElementById(key.join("-"));
+      var url = `?${key[1]}/${key[2]}`;
+      history.replaceState({}, `${key[2]}`, url);
+    }
+    x.classList.remove("opblock-hidden");
+
+    var selected = document.getElementsByClassName("active-tab")[0];
+    if (selected) {
+      selected.classList.remove("active-tab");
+    }
+    var active = document.getElementById(activeId);
+    if (active) {
+      active.classList.add("active-tab");
+    }
   }
 
   static propTypes = {
@@ -115,9 +143,101 @@ export default class Operations extends React.Component {
     }
 
     return (
-      <div>
+      <div className="main-container">
 
-        <aside className='sidebar'>
+        <aside className='sidebar-black' key="sidebar-black">
+          <section className="search-section">
+          <div  className={this.state.searching ? "black-search-box black-searching":"black-search-box"}> 
+          <input type="text" value={this.state.searchValue} onChange={this.handleSearch} placeholder={this.state.searchPlaceHolder}
+onFocus={this.handleSearchFocus} onBlur={this.handleSearchBlur}></input>
+          <button className="search-close" onClick={this.handleSearchClose}>X</button>
+          </div>
+
+          
+          <div className={this.state.searching ? "search-list":"opblock-hidden"}>
+            <ul > 
+              {
+                taggedOps.map((tagObj, tag) => {
+                  let operations = tagObj.get("operations")
+                  let tagDescription = tagObj.getIn(["tagDetails", "description"], null)
+                  let isShownKey = ["operations-tag", createDeepLinkPath(tag)]
+                  let showTag = layoutSelectors.isShown(isShownKey)
+                  return (
+                    operations.map(op => {
+
+                      const path = op.get("path", "")
+                      const method = op.get("method", "")
+                      const operationId =
+                        op.getIn(["operation", "operationId"]) || op.getIn(["operation", "__originalOperationId"]) || opId(op.get("operation"), path, method) || op.get("id")
+
+                      const isShownKey = ["operations", createDeepLinkPath(tag), createDeepLinkPath(operationId)]
+                      let showOp = layoutSelectors.isShown(isShownKey)
+                      if (showOp) {
+                        somethingShown = true;
+                      }
+                      return (
+                        <li className={isShownKey[2].toLowerCase().includes(this.state.searchValue.toLowerCase()) ? "" : "opblock-hidden"} key={"search-" + operationId} name={"search-" + operationId}>
+                          <a onClick={this.showContent.bind(this, isShownKey, "sidebar-" + isShownKey.join("-"), "sidebar-" + tag)} id={"sidebar-" + isShownKey.join("-")}>
+                            {operationId}
+                          </a>
+                        </li>
+
+                      )
+                    }).toArray())
+                })
+              }
+            </ul>
+            </div>
+</section>
+          <div className={window.location.href.indexOf("?info") > -1 ? "tab-black active-tab" : "tab-black"} id="sidebar-info">
+
+            <div className="tab-icon" onClick={this.showContent.bind(this, "info", "sidebar-info", "sidebar-info")} >
+              <i className="fa fa-home" aria-hidden="true"></i>
+              <h6 className="tab-title">Overview</h6>
+            </div>
+          </div>
+          {
+            taggedOps.map((tagObj, tag) => {
+
+              let operations = tagObj.get("operations")
+              let icon = tagObj.getIn(["tagDetails", "icon"], null)
+              return (
+                <div className={window.location.href.indexOf(`?${tag}/`) > -1 ? "tab-black active-tab" : "tab-black"} id={"sidebar-" + tag} key={"sidebar-" + tag}>
+
+                  <div className="tab-icon">
+                    <i className={icon ? icon : "fa fa-cogs"} aria-hidden="true"></i>
+                    <h6 className="tab-title">{tag}</h6>
+                  </div>
+
+                  <ul className="tab-sub-items">
+                    {
+                      operations.map(op => {
+                        const path = op.get("path", "")
+                        const method = op.get("method", "")
+                        const operationId =
+                          op.getIn(["operation", "operationId"]) || op.getIn(["operation", "__originalOperationId"]) || opId(op.get("operation"), path, method) || op.get("id")
+                        const isShownKey = ["operations", createDeepLinkPath(tag), createDeepLinkPath(operationId)]
+                        let showOp = window.location.href.indexOf(`?${isShownKey[1]}/${isShownKey[2]}`) > -1//layoutSelectors.isShown(isShownKey)
+                        if (showOp) {
+                          somethingShown = true;
+                        }
+                        return (
+                          <li key={"sidebar-" + isShownKey.join("-")}>
+                            <a onClick={this.showContent.bind(this, isShownKey, "sidebar-" + isShownKey.join("-"), "sidebar-" + tag)} id={"sidebar-" + isShownKey.join("-")} >
+                              {operationId}
+                            </a>
+                          </li>
+                        )
+                      }).toArray()
+                    }
+                  </ul>
+                </div>
+              )
+            }).toArray()
+          }
+        </aside>
+
+        {/* <aside className='sidebar '>
           <div  className={this.state.searching ? "search-box searching":"search-box"}> 
           <input type="text" value={this.state.searchValue} onChange={this.handleSearch} placeholder={this.state.searchPlaceHolder}
 onFocus={this.handleSearchFocus} onBlur={this.handleSearchBlur}></input>
@@ -127,8 +247,12 @@ onFocus={this.handleSearchFocus} onBlur={this.handleSearchBlur}></input>
             <a className={window.location.href.indexOf("?info")>-1 ? "sidebar-index sidebar-root-item sidebar-root-item-button" : "sidebar-root-item sidebar-root-item-button"} onClick={show.bind(null, "info", "sidebar-info")} id="sidebar-info">Overview		</a>
             {
               taggedOps.map((tagObj, tag) => {
+
                 let operations = tagObj.get("operations")
                 let tagDescription = tagObj.getIn(["tagDetails", "description"], null)
+                let icon = tagObj.getIn(["tagDetails", "description"], null)
+
+                console.log(icon)
                 let isShownKey = ["operations-tag", createDeepLinkPath(tag)]
                 let showTag = window.location.href.indexOf(`?${isShownKey[1]}`)>-1//layoutSelectors.isShown(isShownKey)
                 return (
@@ -199,7 +323,7 @@ onFocus={this.handleSearchFocus} onBlur={this.handleSearchBlur}></input>
 
             }
           </div>
-        </aside>
+        </aside> */}
 
 
         <section className="main-section">
